89c9cf2772f72dc097b8ee883447c6f2a2d4e041
woongbin <woongbin96@naver.com>
2022-08-01 08:59:50 +0900

    create domain

---
+++ src/main/java/com/toybin/api/domain/Post.java
@@ -0,0 +1,29 @@
+package com.toybin.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String title;
+
+    @Lob
+    public String content;
+
+    @Builder
+    public Post(String title, String content){
+        this.title = title;
+        this.content= content;
+    }
+}
