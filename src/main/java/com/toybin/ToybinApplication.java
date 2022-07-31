fc7fff47336b281965a097fa9c093b5940a762e0
woongbin <woongbin96@naver.com>
2022-08-01 08:48:53 +0900

    Initial commit

---
+++ src/main/java/com/toybin/ToybinApplication.java
@@ -0,0 +1,13 @@
+package com.toybin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToybinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToybinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toybin/ToybinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toybin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToybinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
