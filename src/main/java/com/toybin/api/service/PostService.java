4919630d6a7e26fec210033baf505f83f50cc6da
woongbin <woongbin96@naver.com>
2022-08-02 09:02:08 +0900

    create write method

--- src/main/java/com/toybin/api/service/PostService.java
+++ src/main/java/com/toybin/api/service/PostService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class PostServic {
+public class PostService {
 
     private final PostRepository postRepository;
 
