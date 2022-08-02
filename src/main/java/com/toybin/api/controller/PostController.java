b3cf3e63f99edf03797933ca5356f0fedc7854b6
woongbin <woongbin96@naver.com>
2022-08-02 09:01:38 +0900

    create PostController

--- src/main/java/com/toybin/api/controller/PostController.java
+++ src/main/java/com/toybin/api/controller/PostController.java
@@ -1,4 +1,27 @@
 package com.toybin.api.controller;
 
+import com.toybin.api.request.PostCreate;
+import com.toybin.api.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public void addPost(@RequestBody @Valid PostCreate postCreate){
+        postService.write(postCreate);
+    }
+
+
 }
