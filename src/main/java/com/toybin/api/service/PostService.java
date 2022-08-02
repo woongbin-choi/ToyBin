08f3df307d094d70fc4f25458f98af2f4d38bf43
woongbin <woongbin96@naver.com>
2022-08-02 09:01:59 +0900

    create write method

--- src/main/java/com/toybin/api/service/PostService.java
+++ src/main/java/com/toybin/api/service/PostService.java
@@ -1,4 +1,27 @@
 package com.toybin.api.service;
 
-public class PostService {
+import com.toybin.api.domain.Post;
+import com.toybin.api.repository.PostRepository;
+import com.toybin.api.request.PostCreate;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostServic {
+
+    private final PostRepository postRepository;
+
+    public void write(PostCreate postCreate){
+        Post post = Post.builder().
+                title(postCreate.getTitle()).
+                content(postCreate.getContent()).
+                build();
+
+        postRepository.save(post);
+    }
+
 }
