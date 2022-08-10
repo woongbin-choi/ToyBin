ee5c09466ac592ee4cce646f4041cc99e863cf3f
woongbin <woongbin96@naver.com>
2022-08-10 18:51:32 +0900

    make PostEditor

---
+++ src/main/java/com/toybin/api/domain/PostEditor.java
@@ -0,0 +1,18 @@
+package com.toybin.api.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostEditor {
+
+    private final String title;
+
+    private final String content;
+
+    @Builder
+    public PostEditor(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
