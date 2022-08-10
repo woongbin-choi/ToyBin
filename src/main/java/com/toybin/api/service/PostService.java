e02d0f1520f7f324e7faf4d7394fa095c8e1e36b
woongbin <woongbin96@naver.com>
2022-08-10 18:51:48 +0900

    PostService insert edit logic

--- src/main/java/com/toybin/api/service/PostService.java
+++ src/main/java/com/toybin/api/service/PostService.java
@@ -1,8 +1,10 @@
 package com.toybin.api.service;
 
 import com.toybin.api.domain.Post;
+import com.toybin.api.domain.PostEditor;
 import com.toybin.api.repository.PostRepository;
 import com.toybin.api.request.PostCreate;
+import com.toybin.api.request.PostEdit;
 import com.toybin.api.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -34,6 +36,18 @@ public class PostService {
                 title(post.getTitle()).
                 content(post.getContent()).
                 build();
+    }
+
+    public void editPost(Long id, PostEdit postEdit){
+        Post post = postRepository.findById(id).
+                orElseThrow(IllegalArgumentException::new);
+
+        PostEditor.PostEditorBuilder postEditorBuilder = post.toEditor();
+
+        PostEditor postEditor = postEditorBuilder.title(postEdit.getTitle()).
+                content(postEdit.getContent()).build();
+
+        post.edit(postEditor);
     }
 
 }
