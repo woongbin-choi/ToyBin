4acfaed719e9851fff9a53ab82ec45b5d9dfaae6
woongbin <woongbin96@naver.com>
2022-08-10 09:06:30 +0900

    PostService 게시글 한 개 조회 테스트

--- src/test/java/com/toybin/api/service/PostServiceTest.java
+++ src/test/java/com/toybin/api/service/PostServiceTest.java
@@ -3,6 +3,7 @@ package com.toybin.api.service;
 import com.toybin.api.domain.Post;
 import com.toybin.api.repository.PostRepository;
 import com.toybin.api.request.PostCreate;
+import com.toybin.api.response.PostResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,6 +47,22 @@ class PostServiceTest {
     }
 
     @Test
+    @DisplayName("게시글 한 개 조회하기")
     void getPost() {
+        //given
+        Post post = Post.builder().
+                title("test title").
+                content("test content").
+                build();
+
+        postRepository.save(post);
+
+        //when
+        PostResponse postResponse = postService.getPost(post.getId());
+
+        //then
+        assertNotNull(postResponse);
+        assertEquals("test title", postResponse.getTitle());
+        assertEquals("test content", postResponse.getContent());
     }
 }
