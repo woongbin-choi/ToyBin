4805ebab0e6151c2540aa84ffdc343b2fc496902
woongbin <woongbin96@naver.com>
2022-08-10 09:02:32 +0900

    PostService 게시글 조회 로직 생성

--- src/main/java/com/toybin/api/service/PostService.java
+++ src/main/java/com/toybin/api/service/PostService.java
@@ -3,6 +3,7 @@ package com.toybin.api.service;
 import com.toybin.api.domain.Post;
 import com.toybin.api.repository.PostRepository;
 import com.toybin.api.request.PostCreate;
+import com.toybin.api.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,17 @@ public class PostService {
                 build();
 
         postRepository.save(post);
+    }
+
+    public PostResponse getPost(Long id){
+        Post post = postRepository.findById(id).
+                orElseThrow(IllegalArgumentException::new);
+
+        return PostResponse.builder().
+                id(post.getId()).
+                title(post.getTitle()).
+                content(post.getContent()).
+                build();
     }
 
 }
